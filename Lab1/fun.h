2bfd0128d66a82aac91b9e37d124a42ad936ea78
Binah <90138250+Binah-Sephirah@users.noreply.github.com>
2022-03-02 01:34:53 +0300

    Create fun.h

---
+++ Lab1/fun.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+namespace fun {
+	void task2(int&, int&);
+	void task2_2(int*, int*);
+
+	void task7(float&, int&);
+	void task7_2(float*, int*);
+
+	void task10(int&, int&);
+	void task10_2(int*, int*);
+}
